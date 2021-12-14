56195e73a43e678afdf5d1bd6638acc397cc2567
jschiarizzi <jschiarizzi@gwu.edu>
2021-12-14 14:50:24 -0500

    words and links

--- herodao/src/components/GoalsContainer.vue
+++ herodao/src/components/GoalsContainer.vue
@@ -1,51 +1,95 @@
 <template>
   <div class="sm:h-112 md:h-112 h-72">
     <div class="flex flex-row justify-evenly">
-    <div class="flex flex-col sm:pt-24 pt-8 sm:space-y-4 space-y-2">
-      <div class="flex justify-center">
-        <img class="flex sm:h-28 h-12" src="@/assets/poolfunds.svg">
-      </div>
-      <div class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl">Pool funds</div>
-      <div class="flex font-avenir justify-center sm:text-2xl text-xs">1 share = $1 = 1 wxDai</div>
-      <div class="flex flex-col sm:space-y-4 space-y-2 sm:pt-8">
-        <img class="flex sm:h-28 h-12" src="@/assets/create.svg">
-        <div class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl">Create</div>
-        <div class="flex font-avenir justify-center sm:text-2xl text-xs">Use pooled funds to illustrate and write an original work</div>
-      </div>
-    </div>
-    <div class="flex flex-col sm:space-y-4 space-y-2 justify-center">
-      <div class="flex font-aAhaWow sm:text-4xl sm:pt-8 md:pt-4 text-lg justify-center">Goals</div>
-      <div class="flex justify-center">
-        <img class="flex sm:h-28 h-12" src="@/assets/pickhero.svg">
-      </div>
-      <div class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl">Pick Hero</div>
-      <div class="flex font-avenir justify-center sm:text-2xl text-xs">From the public domain</div>
-      <div class="flex flex-col sm:space-y-4 space-y-2 sm:pt-8">
-        <img class="flex sm:h-28 h-12" src="@/assets/print.svg">
-        <div class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl">Print</div>
-        <div class="flex font-avenir justify-center sm:text-2xl text-xs">Print physical copies for DAO members + sales</div>
-      </div>
-    </div>
       <div class="flex flex-col sm:pt-24 pt-8 sm:space-y-4 space-y-2">
         <div class="flex justify-center">
-          <img class="flex sm:h-28 h-12" src="@/assets/reboot.svg">
+          <img class="flex sm:h-28 h-12" src="@/assets/poolfunds.svg" />
         </div>
-        <div class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl">Reboot</div>
-        <div class="flex font-avenir justify-center sm:text-2xl text-xs">Vote on how to give new life to a hero</div>
+        <div
+          class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl"
+        >
+          Pool funds
+        </div>
+        <div class="flex font-avenir justify-center sm:text-2xl text-xs">
+          Work together to raise<br />
+          money from the community
+        </div>
         <div class="flex flex-col sm:space-y-4 space-y-2 sm:pt-8">
-          <img class="flex sm:h-28 h-12" src="@/assets/distribute.svg">
-          <div class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl">Distribute</div>
-          <div class="flex font-avenir justify-center sm:text-2xl text-xs">DAO members get comics, and profits go back to the DAO</div>
+          <img class="flex sm:h-28 h-12" src="@/assets/create.svg" />
+          <div
+            class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl"
+          >
+            Create
+          </div>
+          <div class="flex font-avenir justify-center sm:text-2xl text-xs">
+            Use pooled funds to illustrate<br />
+            and write an original work
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col sm:space-y-4 space-y-2 justify-center">
+        <div
+          class="flex font-aAhaWow sm:text-4xl sm:pt-8 md:pt-4 text-lg justify-center"
+        >
+          Goals
+        </div>
+        <div class="flex justify-center">
+          <img class="flex sm:h-28 h-12" src="@/assets/pickhero.svg" />
+        </div>
+        <div
+          class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl"
+        >
+          Pick Hero
+        </div>
+        <div class="flex font-avenir justify-center sm:text-2xl text-xs">
+          Members vote on which<br />
+          hero gets a new story
+        </div>
+        <div class="flex flex-col sm:space-y-4 space-y-2 sm:pt-8">
+          <img class="flex sm:h-28 h-12" src="@/assets/print.svg" />
+          <div
+            class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl"
+          >
+            Print
+          </div>
+          <div class="flex font-avenir justify-center sm:text-2xl text-xs">
+            Print physical copies<br />
+            for DAO members
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col sm:pt-24 pt-8 sm:space-y-4 space-y-2">
+        <div class="flex justify-center">
+          <img class="flex sm:h-28 h-12" src="@/assets/reboot.svg" />
+        </div>
+        <div
+          class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl"
+        >
+          Reboot
+        </div>
+        <div class="flex font-avenir justify-center sm:text-2xl text-xs">
+          Vote on how to give<br />
+          new life to a hero
+        </div>
+        <div class="flex flex-col sm:space-y-4 space-y-2 sm:pt-8">
+          <img class="flex sm:h-28 h-12" src="@/assets/distribute.svg" />
+          <div
+            class="flex font-architects-daughter justify-center font-extrabold sm:text-2xl"
+          >
+            Grow
+          </div>
+          <div class="flex font-avenir justify-center sm:text-2xl text-xs">
+            The first fan and creator<br />
+            owned super hero franchise.
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-
 <script>
-
 export default {
   name: "GoalsContainer",
-}
+};
 </script>
--- herodao/src/components/Navbar.vue
+++ herodao/src/components/Navbar.vue
@@ -1,30 +1,31 @@
 <template>
   <div class="absolute sm:left-8 left-3 sm:top-3 top-4 overflow-auto">
-    <img class="flex md:h-12 sm:h-10 h-8" src="@/assets/herodaologo.svg">
+    <img class="flex md:h-12 sm:h-10 h-8" src="@/assets/herodaologo.svg" />
   </div>
   <div class="absolute sm:left-20 pl-2 left-10 top-7">
-    <img class="flex md:h-4 sm:h-4 h-3" src="@/assets/herodaoname.svg">
+    <img class="flex md:h-4 sm:h-4 h-3" src="@/assets/herodaoname.svg" />
   </div>
-  <select class="absolute appearance-none sm:text-2xl text-xs sm:left-60 left-36 sm:top-6 top-7 font-avenir font-normal bg-white">
-    <option value="Menu 1">Menu 1 &or;</option>
-    <option value="Submenu 1">Submenu 1</option>
-    <option value="Submenu 2">Submenu 2</option>
-    <option value="Submenu 3">Submenu 3</option>
-    <option value="Submenu 4">Submenu 4</option>
+  <select
+    class="absolute appearance-none sm:text-2xl text-xs sm:left-60 left-36 sm:top-6 top-7 font-avenir font-normal bg-white"
+  >
+    <option value="Menu 1">Guides &or;</option>
+    <option value="Submenu 1">What is Hero DAO</option>
+    <option value="Submenu 2">How to join</option>
+    <option value="Submenu 3">What is Moon Rock</option>
   </select>
-  <select class="absolute appearance-none sm:text-2xl text-xs sm:left-98 left-52 sm:top-6 top-7 font-avenir font-normal bg-white">
-    <option value="Menu 2">Menu 2 &or;</option>
-    <option value="Submenu 1">Submenu 1</option>
-    <option value="Submenu 2">Submenu 2</option>
-    <option value="Submenu 3">Submenu 3</option>
-    <option value="Submenu 4">Submenu 4</option>
+  <select
+    class="absolute appearance-none sm:text-2xl text-xs sm:left-98 left-52 sm:top-6 top-7 font-avenir font-normal bg-white"
+  >
+    <option value="Menu 2">Characters &or;</option>
+    <option value="Submenu 1">Moon Girl</option>
+    <option value="Submenu 2">Trash Man</option>
+    <option value="Submenu 3">Submit a new character</option>
   </select>
-  <select class="absolute appearance-none sm:text-2xl text-xs sm:left-110 left-68 sm:top-6 top-7 font-avenir font-normal bg-white">
-    <option value="Menu 3">Menu 3 &or;</option>
-    <option value="Submenu 1">Submenu 1</option>
-    <option value="Submenu 2">Submenu 2</option>
-    <option value="Submenu 3">Submenu 3</option>
-    <option value="Submenu 4">Submenu 4</option>
+  <select
+    class="absolute appearance-none sm:text-2xl text-xs sm:left-110 left-68 sm:top-6 top-7 font-avenir font-normal bg-white"
+  >
+    <option value="Menu 3">News &or;</option>
+    <option value="Submenu 1">Press</option>
   </select>
   <div class="flex sm:h-2"></div>
 </template>
@@ -32,5 +33,5 @@
 <script>
 export default {
   name: "Navbar",
-}
+};
 </script>
