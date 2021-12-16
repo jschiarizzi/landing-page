e0dd9f870257ef616e26f70d555ded189f97b257
jschiarizzi <jschiarizzi@gwu.edu>
2021-12-15 21:36:18 -0500

    values

--- herodao/src/components/Navbar.vue
+++ herodao/src/components/Navbar.vue
@@ -9,7 +9,9 @@
     class="absolute appearance-none sm:text-2xl text-xs sm:left-60 left-36 sm:top-6 top-7 font-avenir font-normal bg-white"
   >
     <option value="Menu 1">Guides</option>
-    <option value="Submenu 1">What is Hero DAO</option>
+    <a @click="setShow" href="https://blog.herodao.org/">
+      <option value="Submenu 1">What is Hero DAO</option>
+    </a>
     <option value="Submenu 2">How to join</option>
     <option value="Submenu 3">What is Moon Rock</option>
   </select>
